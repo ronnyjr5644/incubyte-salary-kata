60408ac770018484c2f6048c42df40aea248ac65
Raunak Raj <raunakraj20092000@gmail.com>
2026-05-11 16:01:53 +0530

    added employee creation validation

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,15 @@ const app = express();
 app.use(express.json());
 
 app.post("/employees", (req, res) => {
-  res.status(201).json(req.body);
+   const { fullName, jobTitle, country, salary } = req.body;
+
+  if (!fullName || !jobTitle || !country || salary === undefined) {
+    return res.status(400).json({
+      message: "All fields are required",
+    });
+  }
+
+  return res.status(201).json(req.body);
 });
 
 export default app;
