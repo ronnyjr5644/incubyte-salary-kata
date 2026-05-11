8c4fb1ecd3db8e07de75e3562fcdeff3754f3cbd
Raunak Raj <raunakraj20092000@gmail.com>
2026-05-11 15:54:43 +0530

    test: added failing test for employee creation endpoint

---
+++ tests/employee/createEmployee.test.ts
@@ -0,0 +1,24 @@
+import request from "supertest";
+import app from "../../src/app";
+
+describe("POST /employees", () => {
+  it("should create an employee", async () => {
+    const response = await request(app)
+      .post("/employees")
+      .send({
+        fullName: "Raunak Raj",
+        jobTitle: "Software Engineer",
+        country: "India",
+        salary: 100000,
+      });
+
+    expect(response.status).toBe(201);
+
+    expect(response.body).toMatchObject({
+      fullName: "Raunak Raj",
+      jobTitle: "Software Engineer",
+      country: "India",
+      salary: 100000,
+    });
+  });
+});
